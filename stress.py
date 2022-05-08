8caa21e57db3708d37f95360c4f7325861467e88
johnnyr <johnny@ultimachine.com>
2022-05-07 23:57:32 -0500

    partially test rectangle

--- stress.py
+++ stress.py
@@ -10,8 +10,7 @@ class Load(object):
     def __init__(
         self,
         W = 250, # Weigth lbs
-        type = static,
-        drop_height_m = 1,
+        drop_height_m = 0,
         d = 0.004 # Distance traveled after impact m
     ):
         self.W = W
@@ -37,16 +36,18 @@ class Load(object):
 
     @property
     def force(self):
-        if self.type == drop:
+        if self.drop_height_m:
             return self.flb
         return self.W
 
 class Material(object):
     def __init__(
         self,
+        name,
         E = 29.5*10**6, # Modulus Elasticity PSI Steel Cold rolled
         Ys = 40*10**3, # Yeild Strength PSI Steel 1020
     ):
+        self.name = name
         self.E = E
         self.Ys = Ys
 
@@ -54,21 +55,26 @@ class Material(object):
 class RectangleBeam(object):
     def __init__(
         self,
+        name,
+        material,
         width, # Width inches
         thickness, # Thickness (diameter) inches
-        core_width, # core width
-        core_thickness, # core thickness (diameter)
         length, # Length inches
-        material,
-        tube = False
+        core_width = 0, # core width
+        core_thickness = 0, # core thickness (diameter)
     ):
+        self.name = name
         self.b = width
         self.d = thickness
         self.h = core_width
         self.t = core_thickness
         self.l = length
-        self.tube = tube
         self.material = material
+        print(str(name) + " Material = " + material.name + ". Modulus Elasticity " + str(material.E) + " PSI. Yeild Strength " + str(material.Ys) + " PSI")
+        msg = "Rectangle Beam " + str(width) + "in Wide " + str(thickness) + "in Thick " + str(length) + "in Long "
+        if self.h:
+            msg += str(self.h) + "x" + str(self.t) + "in Hollow Core"
+        print(msg)
 
     @property
     def y(self):
@@ -76,12 +82,12 @@ class RectangleBeam(object):
     
     @property
     def A(self): # Area
-        if self.tube:
+        if self.h:
             return self.b * self.d - self.h * self.t
         return self.b * self.d
     @property
     def I(self): # Moment of Inertia
-        if self.tube:
+        if self.h:
             return ( (self.b * self.d**3) - (self.h * self.t**3) ) / 12
         return (self.b * self.d**3) / 12
 
@@ -91,7 +97,7 @@ class RectangleBeam(object):
 
     @property
     def k(self): # Radius of Gyration
-        if self.tube:
+        if self.h:
             return sqrt( (self.b * self.d**3 - self.h * self.t**3) / (12 * (self.b * self.d - self.h * self.t) ) )
         return self.d / (sqrt(12))
 
@@ -99,35 +105,41 @@ class RectangleBeam(object):
 class CircleBeam(object):
     def __init__(
         self,
-        D = 2, # Thickness (diameter) inches
-        d = 1.76, # core thickness (diameter)
-        l = 12, # Length inches
-        tube = False,
-        material = None,
+        name,
+        material,
+        D, # Thickness (diameter) inches
+        l, # Length inches
+        d = 0, # core thickness (diameter)
     ):
+        self.name = name
         self.D = D
         self.d = d
         self.l = l
-        self.tube = tube
         self.material = material
+        print(str(name) + " Material = " + material.name + ". Modulus Elasticity " + str(material.E) + " PSI. Yeild Strength " + str(material.Ys) + " PSI")
+        msg = "Circle Beam " + str(D) + "in Diameter " + str(l) + "in Long "
+        if self.d:
+            msg + str(d) + "in Hollow Core"
+        print(msg)
+        print("Material = " + material.name)
 
     @property
     def y(self):
-        if self.tube:
+        if self.d:
             return self.D / 2
-        return self.d / 2 # Distance from Neutral Axis to Extreme Fiber
+        return self.D / 2 # Distance from Neutral Axis to Extreme Fiber
     
     @property
     def A(self): # Area of Section
-        if self.tube:
+        if self.D:
             return (pi * (self.D^2 - self.d**2))/4
-        return (pi * self.d**2)/4
+        return (pi * self.D**2)/4
 
     @property
     def I(self): # Moment of Inertia
         if self.tube:
             return (pi * (self.D**4 - self.d**4))/64
-        return (pi * self.d**4) / 64
+        return (pi * self.D**4) / 64
     
     @property
     def Z(self): # Section Modulus
@@ -137,10 +149,17 @@ class CircleBeam(object):
     def k(self): # Radius of Gyration
         if self.tube:
             return sqrt( (self.D**2 + self.d**2) / 4 )
-        return self.d / 4
+        return self.D / 4
 
-one_end_fixed = 'one_end_fixed' # other end load
-supported_both_ends = 'supported_both_ends'
+class Object(object):
+    def __init__(
+        self,
+        name
+    ):
+        self.name = name
+
+one_end_fixed = Object('one_end_fixed') # other end load
+supported_both_ends = Object('supported_both_ends')
 
 class Stress(object):
     def __init__(
@@ -156,6 +175,16 @@ class Stress(object):
         self.x = x
         if not self.x:
             self.x = 0.5 * beam.l
+        msg = "Beam " + beam.name + " Load = " + str(load.W) + "lbs"
+        if load.drop_height_m:
+            msg += ". Drop Height " + str(load.drop_height_m) + "m. Distance after impact " + str(load.d)
+        print(msg)
+        print("Support = " + support.name)
+        print("Stress Max " + str(self.stress_max) + " lbs/in^2")
+        print("Maximum deflection " + str(self.deflection_max) + "in")
+        print("Stress at " + str(self.x) + "in from support " + str(self.stress_max) + " lbs/in^2")
+        print("Deflection at " + str(self.x) + "in from support = " + str(self.stress_at_x) + " lbs/in^2")
+        print("Safety Margin " + str(self.margin))
     
     @property
     def stress_at_x(self): # s
@@ -181,29 +210,33 @@ class Stress(object):
     def margin(self): # Safety Margin
         return self.beam.material.Ys / self.stress_max
 
-steel_1020 = Material(29.5*10**6, 40*10**3) #  # Modulus Elasticity PSI, Yeild Strength PSI Steel 1020
-stainless_304 = Material(28*10**6, 31.2*10**3)
-aluminum_6061 = Material(10.2*10**6, 35*10**3)
+steel_1020 = Material('steel_1020', 29.5*10**6, 40*10**3) #  # Modulus Elasticity PSI, Yeild Strength PSI Steel 1020
+stainless_304 = Material('stainless_304', 28*10**6, 31.2*10**3)
+aluminum_6061 = Material('aluminum_6061', 10.2*10**6, 35*10**3)
 
 rocker = RectangleBeam(
+    'Rocker',
+    material = steel_1020,
     width = 2,
     thickness = 2,
-    core_width = 1.76,
-    core_thickness = 1.76,
     length = 12,
-    material = steel_1020,
-    tube = True
+    core_width = 1.76,
+    core_thickness = 1.76
 )
 rocker_load = Load(300)
 rocker_static = Stress(rocker, rocker_load, one_end_fixed)
-drop_load = Load(10, drop)
+print()
+drop_load = Load(10, 1)
 rocker_drop = Stress(rocker, drop_load, one_end_fixed)
+print()
 knuckle_flat = RectangleBeam(
+    'Knuckle Flat',
+    material = steel_1020,
     width = 2,
     thickness = 3/8,
-    length = 1.5,
-    material = steel_1020,
-    tube = False
+    length = 1.5
 )
 knuckle_flat_static = Stress(knuckle_flat, rocker_load, one_end_fixed)
-print(knuckle_flat_static)
+print()
+knuckle_drop = Stress(knuckle_flat, drop_load, one_end_fixed)
+print(str(knuckle_flat.A))
