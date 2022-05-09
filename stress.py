921126d16e57d84e941fc3a397f130824a4c2221
johnnyr <johnny@ultimachine.com>
2022-05-09 09:43:10 -0500

    add supported at ends

--- stress.py
+++ stress.py
@@ -121,23 +121,22 @@ class CircleBeam(object):
         if self.d:
             msg + str(d) + "in Hollow Core"
         print(msg)
-        print("Material = " + material.name)
+        msg = "Area of Section " + str(self.A) + ". Moment of Inertia " + str(self.I) + ". Section Modulus " + str(self.Z) + ". Radius pf Gyration " + str(self.k)
+        print(msg)
 
     @property
     def y(self):
-        if self.d:
-            return self.D / 2
         return self.D / 2 # Distance from Neutral Axis to Extreme Fiber
     
     @property
     def A(self): # Area of Section
-        if self.D:
+        if self.d:
             return (pi * (self.D^2 - self.d**2))/4
         return (pi * self.D**2)/4
 
     @property
     def I(self): # Moment of Inertia
-        if self.tube:
+        if self.d:
             return (pi * (self.D**4 - self.d**4))/64
         return (pi * self.D**4) / 64
     
@@ -147,7 +146,7 @@ class CircleBeam(object):
     
     @property
     def k(self): # Radius of Gyration
-        if self.tube:
+        if self.d:
             return sqrt( (self.D**2 + self.d**2) / 4 )
         return self.D / 4
 
@@ -159,7 +158,8 @@ class Object(object):
         self.name = name
 
 one_end_fixed = Object('one_end_fixed') # other end load
-supported_both_ends = Object('supported_both_ends')
+supported_both_ends = Object('supported_both_ends') # supported both ends, load at center
+support_options = [one_end_fixed, supported_both_ends]
 
 class Stress(object):
     def __init__(
@@ -179,6 +179,9 @@ class Stress(object):
         if load.drop_height_m:
             msg += ". Drop Height " + str(load.drop_height_m) + "m. Distance after impact " + str(load.d)
         print(msg)
+        if support not in support_options:
+            print("No Support selected")
+            return 
         print("Support = " + support.name)
         print("Stress Max " + str(self.stress_max) + " lbs/in^2")
         print("Maximum deflection " + str(self.deflection_max) + "in")
@@ -190,21 +193,28 @@ class Stress(object):
     def stress_at_x(self): # s
         if self.support == one_end_fixed:
             return (self.load.force / self.beam.Z) * (self.beam.l - self.x)
+            return (self.load.force * self.beam.x) / (2 * self.beam.Z)
 
     @property
     def deflection_at_x(self): # y
         if self.support == one_end_fixed:
             return ( (self.load.force * self.x**2) / (6 * self.beam.material.E * self.beam.I) ) * (3 * self.beam.l - self.x)
+        if self.support == supported_both_ends:
+            return ( (self.load.force * self.x) / (48 * self.beam.material.E * self.beam.I) ) * (3 * self.beam.l**2 - 4 * self.x**2)
     
     @property
     def stress_max(self): # stress at critical points
         if self.support == one_end_fixed: # at support
             return (self.load.force * self.beam.l) / self.beam.Z
+        if self.support == supported_both_ends: # at center
+            return -(self.load.force * self.beam.l) / (4 * self.beam.Z)
     
     @property
     def deflection_max(self): # d
         if self.support == one_end_fixed: # at end
-            return (self.load.W * self.beam.l**3) / (3 * self.beam.material.E * self.beam.I)
+            return (self.load.force * self.beam.l**3) / (3 * self.beam.material.E * self.beam.I)
+        if self.support == supported_both_ends: # at load / center
+            return (self.load.force * self.beam.l**3) / (48 * self.beam.material.E * self.beam.I)
     
     @property
     def margin(self): # Safety Margin
@@ -214,8 +224,8 @@ steel_1020 = Material('steel_1020', 29.5*10**6, 40*10**3) #  # Modulus Elasticit
 stainless_304 = Material('stainless_304', 28*10**6, 31.2*10**3)
 aluminum_6061 = Material('aluminum_6061', 10.2*10**6, 35*10**3)
 
-rocker = RectangleBeam(
-    'Rocker',
+rocker_half = RectangleBeam( # One half of rocker calculated from pivot to knuckle
+    'Rocker_half',
     material = steel_1020,
     width = 2,
     thickness = 2,
@@ -224,9 +234,22 @@ rocker = RectangleBeam(
     core_thickness = 1.76
 )
 rocker_load = Load(300)
-rocker_static = Stress(rocker, rocker_load, one_end_fixed)
+rocker_half_static = Stress(rocker_half, rocker_load, one_end_fixed)
 print()
 drop_load = Load(10, 1)
+rocker_half_drop = Stress(rocker_half, drop_load, one_end_fixed)
+print()
+rocker = RectangleBeam(
+    'Rocker',
+    material = steel_1020,
+    width = 2,
+    thickness = 2,
+    length = 24,
+    core_width = 1.76,
+    core_thickness = 1.76
+)
+rocker_static = Stress(rocker, rocker_load, one_end_fixed)
+print()
 rocker_drop = Stress(rocker, drop_load, one_end_fixed)
 print()
 knuckle_flat = RectangleBeam(
@@ -239,4 +262,12 @@ knuckle_flat = RectangleBeam(
 knuckle_flat_static = Stress(knuckle_flat, rocker_load, one_end_fixed)
 print()
 knuckle_drop = Stress(knuckle_flat, drop_load, one_end_fixed)
-print(str(knuckle_flat.A))
+print()
+solid_circle = CircleBeam(
+    'solid_circle',
+    steel_1020,
+    9/16,
+    2.5
+)
+solid_circle_static = Stress(solid_circle, rocker_load, one_end_fixed)
+print()
